63c276e897dfb0c76839facf286c780390509430
christopher.hurley <christopher.hurley@NEITD-123525>
2018-01-22 21:41:46 -0600

    ayyyy

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    // whatever i want
+
      class Program
     {
+        /*
+        * as many lines as i want
+         */
         static void Main( string[] args )
         {
-
             Console.WriteLine("Hello world!");
         }
     }

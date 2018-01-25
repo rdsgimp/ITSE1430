6f67e6dbb5c6a2b7825306de699e56d22be4d563
christopher.hurley <christopher.hurley@NEITD-123525>
2018-01-24 21:41:34 -0600

    wed eve

---
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using dt = System.DateTime;
+
+namespace Nile.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            
+        }
+
+        static void PlayingWithPrimitives()
+        {
+            //Primitive
+            var unitPrice = 10.5m;
+
+            //real decl
+            System.Decimal unitPrice2 = 10.66m;
+
+            //Current thyme
+            dt dateTime = dt.Now;
+
+            System.Collections.ArrayList ayyy;
+        }
+
+        static void PlayingWithVariables()
+        {
+            //single decl
+            int hours = 0;
+            double rate = 10.25;
+
+
+            //still not assigned
+            //if (false)
+            //    hours = 0;
+
+            int hours2 = hours;
+
+            //multi decl
+            string firstName, lastName;
+
+            //string @class;
+
+
+            firstName = "Bob";
+            lastName = "Miller";
+
+            firstName = lastName = "Sue";
+
+            //math ops
+            int x = 0, y = 10;
+            int add = x + y;
+            int subtract = x - y;
+            int multiply = x * y;
+            int divide = x / y;
+            int modulus = x % y;
+
+            // x = x + 10
+            x += 10;
+            double ceiling = Math.Ceiling(rate);
+            double floor = ceiling;
+        }
+    }
+
+}
---
+++ Classwork/Section1/Nile.Host/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nile.Host")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nile.Host")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06a5e318-04c9-4599-82d8-6292fb4efa0a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

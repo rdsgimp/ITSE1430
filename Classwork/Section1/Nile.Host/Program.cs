b0dea0c09ac7cad654154246b0a56d68128d8007
christopher.hurley <christopher.hurley@NEITD-123525>
2018-01-29 21:38:22 -0600

    ayyyyy

--- Classwork/Section1/Nile.Host/Program.cs
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -12,8 +12,126 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
-            
+            bool quit = false;
+            while (!quit)
+            {
+                // display menu
+                char choice = DisplayMenu();
+                // process menu selection    
+                switch (choice)
+                {
+                    case 'l':
+                    case 'L': ListProducts(); break;
+                    case 'a':
+                    case 'A': AddProduct(); break;
+                    case 'q':
+                    case 'Q': quit = true; break;
+                };
+            };
         }
+
+        static void AddProduct()
+        {
+            // get name
+             _name = ReadString("Enter name: ", true);
+
+            // get price
+             _price = ReadDecimal("Enter price: ", 0);
+
+            // get desc
+             _description = ReadString("Enter optional description: ", false);
+        }
+
+        private static decimal ReadDecimal( string message, decimal minValue )
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                //decimal result;
+
+                if (Decimal.TryParse(value, out decimal result))
+                {
+
+                    // if not required or not empty
+                    if (result >= minValue || value != null)
+                        return result;
+                }
+                Console.WriteLine("Value must be greater than or equal to: {0}", minValue);
+                //Console.WriteLine("Value must be greater than or equal to: " + minValue);
+                Console.WriteLine("geet this here" + result);
+            } while (true);
+            // out param result is now out of scope
+            // Console.WriteLine("geet this here" + result);
+
+        }
+
+        private static string ReadString(string message, bool isRequired)
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                // if not required or not empty
+                if (!isRequired || value != "")
+                    return value;
+                Console.WriteLine("Value is required");
+            } while (true);
+        }
+
+        private static char DisplayMenu()
+        {
+
+            do
+            {
+
+                Console.WriteLine("L)ist Products");
+                Console.WriteLine("A)dd Product");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+
+                if (input == "L" || input == "l")
+                {
+                    return input[0];
+                } else if (input == "A" || input == "a" )
+                {
+                    return input[0];
+                } else if (input == "Q" || input == "q")
+                {
+                    return input[0];
+                }
+
+                Console.WriteLine("Please choose a valid option");
+            } while (true);
+
+        }
+
+        private static void ListProducts()
+        {
+            // are there any prod
+            if (_name != null && _name != "")
+            {
+                // display a product
+                Console.WriteLine(_name);
+                Console.WriteLine(_price);
+                Console.WriteLine(_description);
+            } else
+                Console.WriteLine("No Products");
+        }
+
+        // data for a product
+        static string _name;
+        static decimal _price;
+        static string _description;
+
+
+
+
 
         static void PlayingWithPrimitives()
         {

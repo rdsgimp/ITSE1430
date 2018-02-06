718653f51ea841fb77ea3700ddbfd544b7df503e
christopher.hurley <christopher.hurley@NEITD-123525>
2018-02-05 21:07:09 -0600

    section2

--- Classwork/Section2/Nile.Windows/MainForm.Designer.cs
+++ Classwork/Section2/Nile.Windows/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Nile.Windows
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- Classwork/Section2/Nile.Windows/Program.cs
+++ Classwork/Section2/Nile.Windows/Program.cs
@@ -16,7 +16,7 @@ namespace Nile.Windows
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }

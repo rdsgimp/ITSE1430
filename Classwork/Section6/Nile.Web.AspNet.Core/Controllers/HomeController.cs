96a0b1779e8c674dd1ca99113d8dc89fb681db63
christopher.hurley <christopher.hurley@NEITD-123525>
2018-05-02 21:24:02 -0500

    convert sec to net standard

---
+++ Classwork/Section6/Nile.Web.AspNet.Core/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nile.Web.AspNet.Core.Models;
+
+namespace Nile.Web.AspNet.Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ Classwork/Section6/Nile.Web.AspNet.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nile.Web.AspNet.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
